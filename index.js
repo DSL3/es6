07ca2e0ac0a796fb5a7d1493a79c22c6a8f27b37
dalton.leonardo <dalton.leonardo@accenture.com>
2018-07-27 10:11:13 -0300

    Section 3, Lecture5 - ES6 classes

---
+++ index.js
@@ -0,0 +1,28 @@
+class Message {
+    
+    constructor(text = '', created = Date.now()){
+        this.text = '';
+        this.created = created;
+    }    
+
+    get created() {
+        return this._created;
+    }
+    set created(created) {
+        if (!created || isNaN(created)) {
+            throw new Error('Invalid created');
+        }    
+        this._created = created;
+    }
+
+    toString() {
+        return `Message created at: ${this.created} - Text: ${this.text}`;
+    } 
+
+}
+
+var emptyMessage = new Message();
+var textMessage = new Message('Yesterday message', Date.now() - 8640);
+
+console.log(String(emptyMessage));
+console.log(String(textMessage));
