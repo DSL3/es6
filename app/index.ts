d565fd9198143cbb7f137beb35ed663a00679959
dalton.leonardo <dalton.leonardo@accenture.com>
2018-08-01 17:32:20 -0300

    TypeScript:Generics - Parte 1

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,7 @@
 import moment from 'moment';
+import Message from './model/message.model';
+import { messageFactory } from './utils';
+
 import './styles/modules/MessageBox.scss';
 import './styles/modules/MessagesArea.scss';
 
@@ -7,23 +10,44 @@ const logo: any = require('./images/especializa_logo.jpg');
 
 console.log('Index started again');
 
-const Message = function(text) {
-  this.text = text;
-  this.created = Date.now();
+(<HTMLBodyElement> document.body).onload = () => {
+  (<HTMLInputElement> document.getElementById('message'))
+  .value = '/giphy hello';
+
+  (<HTMLButtonElement> document.getElementById('send')).click();
 };
 
-(<HTMLButtonElement> document.getElementById('send')).onclick = () => {
-  const m = new Message(
-    (<HTMLInputElement> document.getElementById('message')).value,
-  );
-  (<HTMLElement> document.getElementById('messages')).innerHTML += template({
-    m,
-    relativeTime: moment(m.created).fromNow(),
-  });
-};
 
-(<HTMLImageElement> document.getElementById('logo')).src = logo;
+  (<HTMLButtonElement> document.getElementById('send')).onclick = () => {
+    messageFactory((<HTMLInputElement> document.getElementById('message')).value)
+    .then((m: Message) => {
+        (<HTMLElement> document.getElementById('messages')).innerHTML += 
+        template({
+          m,
+          relativeTime: moment(m.created).fromNow(),
+        });
+    }); 
+  };
+  
+  (<HTMLImageElement> document.getElementById('logo')).src = logo;
+  
+  if (module && module.hot) {
+    module.hot.accept();
+  }
 
-if (module && module.hot) {
-  module.hot.accept();
-}
+
+// const Message = function(text) {
+//   this.text = text;
+//   this.created = Date.now();
+// };
+
+// (<HTMLButtonElement> document.getElementById('send')).onclick = () => {
+//   const m = new Message(
+//     (<HTMLInputElement> document.getElementById('message')).value,
+//   );
+//   (<HTMLElement> document.getElementById('messages')).innerHTML += template({
+//     m,
+//     relativeTime: moment(m.created).fromNow(),
+//   });
+// };
+
